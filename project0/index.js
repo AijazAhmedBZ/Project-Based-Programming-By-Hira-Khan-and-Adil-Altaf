ccea55a78daaf751052a2fecfb82cb170abe5583
Aijaz Ahmed <aijaz.ahmed.bz@gmail.com>
2023-04-10 09:09:01 +0500

    updated on 10-Apr-23

---
+++ project0/index.js
@@ -0,0 +1,3 @@
+//alert("Aoa from Aijaz");
+// prompt always return string
+console.log(prompt("What is your name?"));
---
+++ project1/script.js
@@ -0,0 +1,16 @@
+// This is the main JS for Project 1.
+// const userName = prompt("Aoa, My name is Aijaz, what is your name?");
+// console.log(userName);
+
+// Bad code
+var X = "Imran";
+
+//Good Code
+
+console.log("before creating variable", teacherName);
+var teacherName = "Imran";
+console.log("after creating variable", teacherName);
+var subject = "JavaScript";
+
+teacherName = "Alexender";
+console.log("after reassigning variable", teacherName);
